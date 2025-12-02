347e01332922e9db928e0bc0bafaa462fedb1d39
mahmoud osman <minimarvels369@gmail.com>
2025-12-02 00:53:01 +0000

    fix: Allow app to be embedded in Shopify admin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "frame-ancestors 'self' *.myshopify.com admin.shopify.com;",
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
